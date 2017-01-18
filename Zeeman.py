90da234a5a687232558b5178e88a9503096b0e13
fstaeg <fabian.staeger@uzh.ch>
2017-01-18 18:54:52 +0100

    Update Zeeman.py

--- Zeeman.py
+++ Zeeman.py
@@ -8,17 +8,22 @@ Created on Tue Jan 17 13:16:03 2017
 
 import numpy as np
 import matplotlib.pyplot as plt
-from excelreadZeeman import machliste
+#from excelreadZeeman import machliste
 
 #-------------------------Farbe---------------------
 #Gelb=1, Blau=0
 
 farbe = 1
 
+print ''
+
 if farbe == 1:
     lam0 = 585.249e-9
     n0 = 1.5145
+    un0 = 1e-4
+    udn = 2767.9
     
+    print 'Farbe: Gelb'
     A2plus = np.array([11.35, 11.36, 11.31, 11.29])*10**(-3)
     A2 = np.array([11.26, 11.16, 11.14, 11.13])*10**(-3)
     A2minus = np.array([10.86, 10.87, 10.77, 10.8])*10**(-3)
@@ -34,7 +39,10 @@ if farbe == 1:
 elif farbe == 0:
     lam0 = 540.056e-9
     n0 = 1.5172
+    un0 = 1e-4
+    udn = 2257.1
     
+    print 'Farbe: Blau-Grün' 
     A2plus = np.array([11.14, 11.07, 11.06, 11.05])*10**(-3)
     A2 = np.array([10.91, 10.85, 10.9, 10.86])*10**(-3)
     A2minus = np.array([10.6, 10.58, 10.54, 10.53])*10**(-3)
@@ -49,34 +57,6 @@ elif farbe == 0:
 
 
 
-#-------------------------A berechnen---------------------
-
-meanA2plus = np.mean(A2plus)
-meanA2 = np.mean(A2)
-meanA2minus = np.mean(A2minus)
-
-meanA1plus = np.mean(A1plus)
-meanA1 = np.mean(A1)
-meanA1minus = np.mean(A1minus)
-
-meanA0plus = np.mean(A0plus)
-meanA0 = np.mean(A0)
-meanA0minus = np.mean(A0minus)
-
-errA2plus = np.std(A2plus)
-errA2 = np.std(A2)
-errA2minus = np.std(A2minus)
-
-errA1plus = np.std(A1plus)
-errA1 = np.std(A1)
-errA1minus = np.std(A1minus)
-
-errA0plus = np.std(A0plus)
-errA0 = np.std(A0)
-errA0minus = np.std(A0minus)
-
-
-
 #-------------------------Daten---------------------
 
 """Spannungsmessung Flipspule"""
@@ -84,15 +64,21 @@ Uplus = np.array([4.3, 4.27, 4.29, 4.32, 4.31, 4.29, 4.33, 4.27, 4.34, 4.31])
 Uminus = np.array([[4.24, 4.23, 4.22, 4.24, 4.22, 4.24, 4.24, 4.24, 4.24, 4.24]])
 meanUplus = np.mean(Uplus)
 meanUminus = np.mean(Uminus)
+stdUplus = np.std(Uplus)
+stdUminus = np.std(Uminus)
 
 U = (meanUplus+meanUminus) / 2
-errU = 0        #Wie machtmerdas??
+uU = (stdUplus + stdUminus) / 2
+
+
 """Drehachse in cm"""
 
 s=[22.95, 23.0, 23.1, 23.05, 22.95]
-s=np.array(s)*10*-2
+s=np.array(s)*10**(-2)
 S=np.mean(s)
-errS=np.std(s)
+uS=np.std(s)
+
+
 
 #------------------------Funktionen------------------
 
@@ -122,11 +108,13 @@ def BFeld(U,F,N):               #U=gemessene Spannung, F=Fläche der Spule N = A
 def gj(deltanu,B,h,muB,deltamj):
     return (h*deltanu)/(muB*B*deltamj)
 
+
+
 #-----------Berechnungen----------
 
 
-
 d=3.213*10**(-3)
+ud = 1e-6
 dn=-(1.525-1.510)/(685-400)*10**9
 c=2.99*10**8
 
@@ -134,26 +122,57 @@ c=2.99*10**8
 spuledurchmesser=19.98*10**(-3)
 drahtdurchmesser=0.06*10**(-3)
 radius=spuledurchmesser/2+drahtdurchmesser/2
-a=radius**2*np.pi
+F=radius**2*np.pi
+uF = np.pi*(drahtdurchmesser/2)**2
 N=127
 
 h=6.626 * 10**(-34)
 muB=9.247 * 10**(-24)
 deltamj=1
 
+
+"""A"""
+
+A2plus = np.mean(A2plus)
+A2 = np.mean(A2)
+A2minus = np.mean(A2minus)
+
+A1plus = np.mean(A1plus)
+A1 = np.mean(A1)
+A1minus = np.mean(A1minus)
+
+A0plus = np.mean(A0plus)
+A0 = np.mean(A0)
+A0minus = np.mean(A0minus)
+
+uA2plus = np.std(A2plus)
+uA2 = np.std(A2)
+uA2minus = np.std(A2minus)
+
+uA1plus = np.std(A1plus)
+uA1 = np.std(A1)
+uA1minus = np.std(A1minus)
+
+uA0plus = np.std(A0plus)
+uA0 = np.std(A0)
+uA0minus = np.std(A0minus)
+
+
+
 """Theta"""
 
-theta2plus = theta(meanA2plus,S)
-theta2 = theta(meanA2,S)
-theta2minus = theta(meanA2minus,S)
+theta2plus = theta(A2plus,S)
+theta2 = theta(A2,S)
+theta2minus = theta(A2minus,S)
 
-theta1plus = theta(meanA1plus,S)
-theta1 = theta(meanA1,S)
-theta1minus = theta(meanA1minus,S)
+theta1plus = theta(A1plus,S)
+theta1 = theta(A1,S)
+theta1minus = theta(A1minus,S)
 
-theta0plus = theta(meanA0plus,S)
-theta0 = theta(meanA0,S)
-theta0minus = theta(meanA0minus,S)
+theta0plus = theta(A0plus,S)
+theta0 = theta(A0,S)
+theta0minus = theta(A0minus,S)
+
 
 
 """M"""
@@ -162,19 +181,24 @@ M2 = Ordnung(d,n0,lam0, theta2)
 M1 = Ordnung(d,n0,lam0, theta1)
 M0 = Ordnung(d,n0,lam0, theta0)
 
+
+
 """deltanu"""
-dnu2 = deltanu(theta2plus,theta2minus, lam0, M2, n0, dn, c, d)
-dnu1 = deltanu(theta1plus,theta1minus, lam0, M2, n0, dn, c, d)
-dnu0 = deltanu(theta0plus,theta0minus, lam0, M2, n0, dn, c, d)
-
-B = BFeld(U,a,N)
-
-lande2 = gj(dnu2,B,h,muB,deltamj)
-lande1 = gj(dnu1,B,h,muB,deltamj)
-lande0 = gj(dnu0,B,h,muB,deltamj)
+deltanu2 = deltanu(theta2plus,theta2minus, lam0, M2, n0, dn, c, d)
+deltanu1 = deltanu(theta1plus,theta1minus, lam0, M2, n0, dn, c, d)
+deltanu0 = deltanu(theta0plus,theta0minus, lam0, M2, n0, dn, c, d)
 
 
-print(lande2,lande1,lande0)
+
+"""Lande-Faktoren"""
+
+B = BFeld(U,F,N)
+
+lande2 = gj(deltanu2,B,h,muB,deltamj)
+lande1 = gj(deltanu1,B,h,muB,deltamj)
+lande0 = gj(deltanu0,B,h,muB,deltamj)
+
+
 
 
 #-------------------------Ableitungen---------------------
@@ -212,23 +236,22 @@ def dnudn0(thetaplus,thetaminus,M,d,n0,dn,c,lam0):
 def dnuddn(thetaplus,thetaminus,M,d,n0,dn,c,lam0):
     return -c*((np.sin(thetaplus))**2-(np.sin(thetaminus))**2)/lam0**2 * 4*n0 * (lam0*M**2/d**2-4*n0*dn)**(-2)
 
-def dBdU(U,A,N):
-    return 1/(314.16*A*N)
+def dBdU(U,F,N):
+    return 1/(314.16*F*N)
 
-def dBdA(U,A,N):
-    return -U/(314.16*A**2*N)
+def dBdF(U,F,N):
+    return -U/(314.16*F**2*N)
 
 def dgdnu(deltanu,muB,B,deltamj):
     return h/(muB*B*deltamj)
 
 def dgdB(deltanu,muB,B,deltamj):
+    return -h*deltanu/(muB*B**2*deltamj)
 
 
 
 #-------------------------Fehler---------------------
 
-def uA(ux):
-    return np.sqrt((ux*1)**2+(ux*(-1))**2)
 
 def utheta(A,S,uA,uS):
     dA = dthetadA(A,S)
@@ -252,7 +275,7 @@ def unu(thetaplus,thetaminus,M,d,n0,dn,c,lam0,uthetaplus,uthetaminus,uM,ud,un0,u
 
 def uB(U,F,N,uU,uF):
     dU = dBdU(U,F,N)
-    dA = dBdA(U,F,N)
+    dF = dBdF(U,F,N)
     return np.sqrt((uU*dU)**2+(uF*dF)**2)
 
 def ug(deltanu,muB,B,deltamj,unu,uB):
@@ -264,26 +287,17 @@ def ug(deltanu,muB,B,deltamj,unu,uB):
 
 #-------------------------Fehlerrechnung---------------------
 
-ux = 5e-6
-uS = np.std(s)
-ud = 1e-6
-un0 = 1e-4
-udn =
-uU =
-uA = uA(ux)
-uF = np.pi*(3e-5)**2
+utheta0 = utheta(A0,S,uA0,uS)
+utheta0plus = utheta(A0plus,S,uA0,uS)
+utheta0minus = utheta(A0minus,S,uA0,uS)
 
-utheta0 = utheta(A0,S,uA,uS)
-utheta0plus = utheta(A0plus,S,uA,uS)
-utheta0minus = utheta(A0minus,S,uA,uS)
+utheta1 = utheta(A1,S,uA1,uS)
+utheta1plus = utheta(A1plus,S,uA1,uS)
+utheta1minus = utheta(A1minus,S,uA1,uS)
 
-utheta1 = utheta(A1,S,uA,uS)
-utheta1plus = utheta(A1plus,S,uA,uS)
-utheta1minus = utheta(A1minus,S,uA,uS)
-
-utheta2 = utheta(A2,S,uA,uS)
-utheta2plus = utheta(A2plus,S,uA,uS)
-utheta2minus = utheta(A2minus,S,uA,uS)
+utheta2 = utheta(A2,S,uA2,uS)
+utheta2plus = utheta(A2plus,S,uA2,uS)
+utheta2minus = utheta(A2minus,S,uA2,uS)
 
 uM0 = uM(d,n0,theta0,lam0,ud,un0,utheta0)
 uM1 = uM(d,n0,theta1,lam0,ud,un0,utheta1)
@@ -295,6 +309,43 @@ unu2 = unu(theta2plus,theta2minus,M2,d,n0,dn,c,lam0,utheta2plus,utheta2minus,uM2
 
 uB = uB(U,F,N,uU,uF)
 
-ug0 = ug(deltanu0,muB,B0,deltamj,unu0,uB)
-ug1 = ug(deltanu1,muB,B1,deltamj,unu1,uB)
-ug2 = ug(deltanu2,muB,B2,deltamj,unu2,uB)
+ug0 = ug(deltanu0,muB,B,deltamj,unu0,uB)
+ug1 = ug(deltanu1,muB,B,deltamj,unu1,uB)
+ug2 = ug(deltanu2,muB,B,deltamj,unu2,uB)
+
+
+
+#-------------------------Print---------------------
+
+print ''
+print 'Theta0+: (', '%.3f' % (theta0plus*180/np.pi), '+/-', '%.3f' % (utheta0plus*180/np.pi), ') °C'
+print 'Theta0: (', '%.3f' % (theta0*180/np.pi), '+/-', '%.3f' % (utheta0*180/np.pi), ') °C'
+print 'Theta0-: (', '%.3f' % (theta0minus*180/np.pi), '+/-', '%.3f' % (utheta0minus*180/np.pi), ') °C'
+print 'Theta1+: (', '%.3f' % (theta1plus*180/np.pi), '+/-', '%.3f' % (utheta1plus*180/np.pi), ') °C'
+print 'Theta1: (', '%.3f' % (theta1*180/np.pi), '+/-', '%.3f' % (utheta1*180/np.pi), ') °C'
+print 'Theta1-: (', '%.3f' % (theta1minus*180/np.pi), '+/-', '%.3f' % (utheta1minus*180/np.pi), ') °C'
+print 'Theta2+: (', '%.3f' % (theta2plus*180/np.pi), '+/-', '%.3f' % (utheta2plus*180/np.pi), ') °C'
+print 'Theta2: (', '%.3f' % (theta2*180/np.pi), '+/-', '%.3f' % (utheta2*180/np.pi), ') °C'
+print 'Theta2-: (', '%.3f' % (theta2minus*180/np.pi), '+/-', '%.3f' % (utheta2minus*180/np.pi), ') °C'
+
+print ''
+print 'Ordnung M0: (', '%.1f' % M0, '+/-', '%.1f' % uM0, ')'
+print 'Ordnung M1: (', '%.1f' % M1, '+/-', '%.1f' % uM1, ')'
+print 'Ordnung M2: (', '%.1f' % M2, '+/-', '%.1f' % uM2, ')'
+
+print ''
+print 'Delta nu (M0): (', '%.2f' % (-deltanu0/1e9), '+/-', '%.2f' % (unu0/1e9), ') GHz'
+print 'Delta nu (M1): (', '%.2f' % (-deltanu1/1e9), '+/-', '%.2f' % (unu1/1e9), ') GHz'
+print 'Delta nu (M2): (', '%.2f' % (-deltanu2/1e9), '+/-', '%.2f' % (unu2/1e9), ') GHz'
+
+print ''
+print 'B-Feld: (', '%.3f' % (B), '+/-', '%.3f' % (uB), ') T'
+
+print ''
+print 'Landé-Faktor (M0): (', '%.3f' % (-lande0), '+/-', '%.3f' % (ug0), ')'
+print 'Landé-Faktor (M1): (', '%.3f' % (-lande1), '+/-', '%.3f' % (ug1), ')'
+print 'Landé-Faktor (M2): (', '%.3f' % (-lande2), '+/-', '%.3f' % (ug2), ')'
+print ''
+
+
+
